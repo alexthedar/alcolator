87b261fd8236bdb9162fbd82d9b10ed6f73dc6c9
alexthedar <alex.castaneda@gmail.com>
2016-05-23 19:04:45 -0700

    storyboard made

---
+++ alocator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alocator
+//
+//  Created by Alexandar on 5/23/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ alocator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  alocator
+//
+//  Created by Alexandar on 5/23/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
