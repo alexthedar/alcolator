44a2a2aaaf9029408b6e0822c5aef767d23966ed
alexthedar <alex.castaneda@gmail.com>
2016-05-24 18:10:16 -0700

    whiskey calculator finished

---
+++ alocator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  alocator
+//
+//  Created by Alexandar on 5/24/16.
+//  Copyright © 2016 Alexandar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
